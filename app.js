b31fc198df1483834f226ff0bf5f2f0a1322dfa0
kimhandole <kimhandole@gmail.com>
2020-01-21 09:38:34 -0800

    add user auth

--- app.js
+++ app.js
@@ -2,12 +2,36 @@ const express = require("express"); // creates new express server
 const mongoose = require("mongoose");
 const app = express();
 const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const User= require("./models/User");
+const bodyParser = require("body-parser");
+
+
 mongoose //connect to Mongoose
-    .connect(db, { useNewUrlParser: true })
+    .connect(db, { useNewUrlParser: true,
+                useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    const user = new User({
+        username: "jim",
+        email: "jim@jim.jim",
+        password: "jimisgreat123"
+    });
+    user.save();
+    res.send("Hello World!");
+});
+
+app.use("/api/users", users);
+
 const port = process.env.PORT || 5000; //which port to run on
 
-app.listen(port, () => console.log(`Server is running on port ${port}`)); //creates a socket to listen for connections
+//creates a socket to listen for connections
+app.listen(port, () => console.log(`Server is running on port ${port}`)); 
