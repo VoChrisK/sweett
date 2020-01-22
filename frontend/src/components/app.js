9693f82790c2a738ca1b2ed5d703bd5b3255af97
kimhandole <kimhandole@gmail.com>
2020-01-22 11:24:45 -0800

    add modal login, signup styles

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -3,18 +3,14 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch, Route } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
 import MainPage from './main/main_page';
-import LoginFormContainer from './session/login_form_container';
-import SignupFormContainer from './session/signup_form_container';
 import DashboardContainer from './dashboard/dashboard_container'
 
 const App = () => (
-  <div>
+  <div id="app">
     <NavBarContainer />
     <Switch>
       <ProtectedRoute exact path="/dashboard" component={DashboardContainer} />
       <AuthRoute exact path="/" component={MainPage} />
-      <AuthRoute exact path="/login" component={LoginFormContainer} />
-      <AuthRoute exact path="/signup" component={SignupFormContainer} />
     </Switch>
   </div>
 );
--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import Modal from '../modal/modal';
+
 // import './navbar.css'
 
 class NavBar extends React.Component {
@@ -27,10 +29,11 @@ class NavBar extends React.Component {
             );
         } else {
             return (
-                <div>
-                    <Link to={'/signup'}>Signup</Link>
-                    <Link to={'/login'}>Login</Link>
-                </div>
+                <nav className="login-signup">
+                    <button onClick={this.props.login}>Login</button>
+                    &nbsp;or&nbsp;
+                    <button onClick={this.props.signup}>Signup</button>
+                </nav>
             );
         }
     }
@@ -38,6 +41,7 @@ class NavBar extends React.Component {
     render() {
         return (
             <div>
+                <Modal />
                 <h1>SWETT</h1>
                 {this.getLinks()}
             </div>
