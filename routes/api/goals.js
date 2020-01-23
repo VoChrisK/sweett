248500a05701e42f402bc3c5079b88053e79d067
Herman <hexie646>
2020-01-23 15:27:14 -0800

    Completes backend for goals, questions, and attempts

---
+++ routes/api/goals.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const router = express.Router();
+const Goal = require("../../models/Goal");
+const validateGoalInput = require("../../validation/goals");
+const passport = require("passport");
+
+//fetch all goals based on category id
+router.get("/category/:category_id", (req, res) => {
+    Goal.find({ category_id: req.params.category_id })
+        .then(goals => res.json(goals))
+        .catch(errors => res.status(400).json({ errors }));
+});
+
+router.get("/:id", (req, res) => {
+    Goal.findById(req.params.id)
+        .then(goal => res.json(goal))
+        .catch(errors => res.status(404).json({ errors }));
+});
+
+// router.get("/", (req, res) => {
+//     Goal.find({})
+//         .then(goals => res.json(goals))
+//         .catch(errors => res.status(404).json({ errors }));
+// });
+
+router.post("/", (req, res) => {
+    const { errors, isValid } = validateGoalInput( req.body );
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+    const goal = new Goal({ description: req.body.description, category_id: req.body.category_id });
+    goal.save()
+        .then(goal => res.json(goal))
+        .catch(errors => res.status(400).json({ errors }));
+});
+
+//updates goal
+router.patch("/:id", (req, res) => {
+    const {errors, isValid} = validateGoalInput( req.body );
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    Goal.findById(req.params.id)
+        .then(goal => {
+            goal.description = req.body.description;
+            goal.progress = req.body.progress;
+            goal.addToTotal = req.body.addToTotal;
+            goal.save()
+                .then(goal => res.json(goal))
+                .catch(errors => res.status(400).json({ errors }));
+        })
+        .catch(errors => res.status(400).json({ errors }));
+});
+
+//deletes goal
+router.delete("/:id", (req, res) => {
+    Goal.deleteOne({ _id: req.params.id })
+        .then(goal => res.json(goal))
+        .catch(errors => res.status(404).json({ errors }));
+});
+
+module.exports = router;
---
+++ routes/api/questions.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const router = express.Router();
+const Question = require("../../models/Question");
+const validateQuestionInput = require("../../validation/questions");
+const passport = require("passport");
+
+//fetch all questions based on category id into an array
+router.get("/category/:category_id", (req, res) => {
+    Question.find({ category_id: req.params.category_id })
+        .then(questions => res.json(questions))
+        .catch(errors => res.status(400).json({ errors }));
+});
+
+//fetches a question object
+router.get("/:id", (req, res) => {
+    Question.findById(req.params.id)
+        .then(question => res.json(question))
+        .catch(errors => res.status(404).json({ errors }));
+});
+
+// testing with a questions index
+// router.get("/", (req, res) => {
+//     Question.find({})
+//         .then(questions => res.json(questions))
+//         .catch(errors => res.status(404).json({ errors }));
+// });
+
+router.post("/", (req, res) => {
+    const { errors, isValid } = validateQuestionInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+    const question = new Question({ 
+        name: req.body.name, 
+        category_id: req.body.category_id, 
+        difficulty: req.body.difficulty 
+    });
+    question.save()
+        .then(question => res.json(question))
+        .catch(errors => res.status(400).json({ errors }));
+});
+
+//updates question
+router.patch("/:id", (req, res) => {
+    const { errors, isValid } = validateQuestionInput(req.body);
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    Question.findById(req.params.id)
+        .then(question => {
+            question.name = req.body.name;
+            question.status = req.body.status;
+            question.difficulty = req.body.difficulty;
+            question.save()
+                .then(question => res.json(question))
+                .catch(errors => res.status(400).json({ errors }));
+        })
+        .catch(errors => res.status(400).json({ errors }));
+});
+
+//deletes question
+router.delete("/:id", (req, res) => {
+    Question.deleteOne({ _id: req.params.id })
+        .then(question => res.json(question))
+        .catch(errors => res.status(404).json({ errors }));
+});
+
+module.exports = router;
