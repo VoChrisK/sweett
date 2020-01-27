c572c7b7f5239073559470fb8ab3a78a6f667037
Brian <xbrianxtranx@gmail.com>
2020-01-27 09:28:58 -0800

    app.js

--- app.js
+++ app.js
@@ -11,13 +11,6 @@ const bodyParser = require("body-parser");
 const passport = require('passport');
 const path = require('path');
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
-    app.get('/', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-    })
-}
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
   app.get("/", (req, res) => {
