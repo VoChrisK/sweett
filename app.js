dccbf5fa77e5b5a67e9e204a664d4e82debc2ec2
Brian <xbrianxtranx@gmail.com>
2020-01-27 09:24:52 -0800

    heroku test

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ const attempts = require("./routes/api/attempts");
 const bodyParser = require("body-parser");
 const passport = require('passport');
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
 mongoose //connect to Mongoose
     .connect(db, { useNewUrlParser: true,
                 useUnifiedTopology: true })
