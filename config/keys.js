8db99a5f675e32f9c2bf4af61c0bbba74be2ff1c
Brian <xbrianxtranx@gmail.com>
2020-01-27 00:21:54 -0800

    heroku test

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://dev:EOmsVoAbVIlqoOZJ@cluster0-vaswg.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "secret"
-};
+
+
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
