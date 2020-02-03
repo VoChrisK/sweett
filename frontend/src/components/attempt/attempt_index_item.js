53a4827f046d1e513cc2e22c3332e7d0c4fa1df7
kimhandole <kimhandole@gmail.com>
2020-02-03 11:56:55 -0800

    add question style

--- frontend/src/components/attempt/attempt_index_item.js
+++ frontend/src/components/attempt/attempt_index_item.js
@@ -5,7 +5,7 @@ const AttemptIndexItem = ({ attempt, deleteAttempt, idx }) => {
     console.log(attempt)
     return (
         <div className="attempt">
-            <strong className="attempt-number">Attempt #{idx+1}</strong>
+            <strong className="attempt-number">Attempt {idx+1}</strong>
             <strong className="attempt-time">{formatTime(attempt.time)}</strong>
             <strong className="attempt-date">{formatDate(attempt.date)}</strong>
         </div>
