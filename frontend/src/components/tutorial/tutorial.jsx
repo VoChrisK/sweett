e70106ce7a78dfa08835ab065a4b1894be02e034
CurrySoup <vo.chris.k@gmail.com>
2020-03-29 19:32:25 -0700

    Add circle visuals to highlight each button for tutorial

--- frontend/src/components/tutorial/tutorial.jsx
+++ frontend/src/components/tutorial/tutorial.jsx
@@ -8,19 +8,22 @@ const Tutorial = ({ hideTutorial }) => {
     </div>
 
     const stepTwo = <div className="tutorial-description two" onClick={e => e.stopPropagation()}>
-            <h1 className="tutorial-message">This button is to add a task! You will be prompted to fill out the name of the task and which section it belongs to.</h1>
+        <img className="circle" src={require("./../../assets/images/circle.png")} />
+        <h1 className="tutorial-message">This button is to add a task! You will be prompted to fill out the name of the task and which section it belongs to.</h1>
     </div>
 
     const stepThree = <div className="tutorial-description three" onClick={e => e.stopPropagation()}>
+        <img className="circle" src={require("./../../assets/images/circle.png")} />
         <h1 className="tutorial-message">Once you add a task, you are able to record its time. This button will start a recording session for this task. You are able to pause and stop the timer.</h1>
     </div>
 
     const stepFour = <div className="tutorial-description four" onClick={e => e.stopPropagation()}>
-        {/* <img className="circle" src={require("./../../assets/images/circle.png")} /> */}
+        <img className="circle" src={require("./../../assets/images/circle.png")} />
         <h1 className="tutorial-message">This button is to add a goal. You will be prompted to add its description, amount to complete it, and choose to track its time.</h1>
     </div>
 
     const stepFive = <div className="tutorial-description five" onClick={e => e.stopPropagation()}>
+        <img className="circle" src={require("./../../assets/images/circle.png")} />
         <h1 className="tutorial-message">This button is to modify the time limit for each task.</h1>
     </div>
 
